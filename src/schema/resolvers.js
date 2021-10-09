92b8d431375528603f6a91d5877662aae06555ac
glauroqj <glauro.juliani@hotmail.com>
2021-10-09 11:26:41 -0300

    add: resolvers done

---
+++ src/schema/resolvers.js
@@ -0,0 +1,26 @@
+import { ApolloError } from 'apollo-server-express'
+
+/** queries */
+import EventQuery from 'datasources/events/eventQuery'
+
+const responseError = error => {
+  const { message, extensions } = error
+
+  if (extensions?.response?.body?.errorMessage) {
+    throw new ApolloError(
+      message + ' - ' + extensions.response.body.errorMessage, 
+      extensions.response.body.errorCode
+    );
+  } else {
+    throw new ApolloError(message, 'No extensions on error');
+  }
+}
+
+const resolvers = {
+  Query: {
+    ...EventQuery(responseError)
+  },
+  Mutation: {}
+}
+
+export default resolvers
